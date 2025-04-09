4a230857b0e1fc9f236459b1cf58ee419c086276
jchyeon0814 <jchyeon0814@gmail.com>
2025-04-10 06:57:17 +0900

    스토리북 정리

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -5,7 +5,7 @@ import { Button } from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Button',
+  title: 'Example/Button', //스토리 그룹화
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
@@ -18,7 +18,7 @@ const meta = {
     backgroundColor: { control: 'color' },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
+  args: { onClick: fn() }, //meta 타입을 사용하는 모든 Story 객체에 공통으로 적용됨
 } satisfies Meta<typeof Button>;
 
 export default meta;
